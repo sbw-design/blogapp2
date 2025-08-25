a0b50de8ac0a64c64a4683da272c94308358e70e
git_ts-admin <24ikomot@gmail.com>
2025-08-25 20:33:58 +0900

    記事を更新できるように変更

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,6 +22,20 @@ class ArticlesController < ApplicationController
     end
   end
   
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to article_path(@article), notice: "更新しました"
+    else
+      flash.now[:error] = "更新に失敗しました"
+      render :edit, status: :unprocessable_entity
+    end
+  end
+  
   private
   def article_params
     params.require(:article).permit(:title, :content)
