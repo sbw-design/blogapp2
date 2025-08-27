70454f95716b93b7682c84d45f76cedf1d21718d
git_ts-admin <24ikomot@gmail.com>
2025-08-27 18:05:48 +0900

    gemにてrubocopのインストールとコンソールより"rubocop -a"自動書き換えを実行

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,51 +1,51 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update]
-  
+
   def index
     # render 'home/index' 書かなくても良い
     @articles = Article.all
   end
-    
+
   def show
   end
-  
+
   def new
     @article = Article.new
   end
-  
+
   def create
     @article = Article.new(article_params)
     if @article.save
-      redirect_to article_path(@article), notice: "保存しました"
+      redirect_to article_path(@article), notice: '保存しました'
     else
-      flash.now[:error] = "保存に失敗しました"
+      flash.now[:error] = '保存に失敗しました'
       render :new, status: :unprocessable_entity
     end
   end
-  
+
   def edit
   end
-  
+
   def update
     if @article.update(article_params)
-      redirect_to article_path(@article), notice: "更新しました"
+      redirect_to article_path(@article), notice: '更新しました'
     else
-      flash.now[:error] = "更新に失敗しました"
+      flash.now[:error] = '更新に失敗しました'
       render :edit, status: :unprocessable_entity
     end
   end
-  
+
   def destroy
     article = Article.find(params[:id])
     article.destroy!
-    redirect_to root_path, status: :see_other, notice: "削除しました"
+    redirect_to root_path, status: :see_other, notice: '削除しました'
   end
-  
+
   private
   def article_params
     params.require(:article).permit(:title, :content)
   end
-  
+
   def set_article
     @article = Article.find(params[:id])
   end
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails/all"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -15,7 +15,7 @@ module Blogapp2
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
-    
+
     # デフォルトの言語を日本語に設定
     config.i18n.default_locale = :ja
 
