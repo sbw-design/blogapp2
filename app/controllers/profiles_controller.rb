0f5de07812a7e8b1ebe87cad9fc87f1d80554796
git_ts-admin <24ikomot@gmail.com>
2025-08-31 16:38:02 +0900

    プロフィールを作成・更新できるよう設定

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -7,6 +7,33 @@ class ProfilesController < ApplicationController
   end
   
   def edit
-    @profile = current_user.build_profile
+    # if current_user.profile.present?
+    #   @profile = current_user.profile
+    # else
+    # @profile = current_user.build_profile
+    # end
+    @profile = current_user.prepare_profile
+  end
+  
+  def update
+    @profile = current_user.prepare_profile
+    @profile.assign_attributes(profile_params)
+    if @profile.save
+      redirect_to profile_path, notice: 'プロフィールを更新しました'
+    else
+      flash.now[:error] = 'プロフィールの更新に失敗しました'
+      render :edit, status: :unprocessable_entity
+    end
+  end
+  
+  private
+  def profile_params
+    params.require(:profile).permit(
+      :nickname,
+      :introduction,
+      :gender,
+      :birthday,
+      :subscribed
+    )
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -34,4 +34,8 @@ class User < ApplicationRecord
   def display_name
     self.email.split('@').first
   end
+  
+  def prepare_profile
+    profile || build_profile
+  end
 end
